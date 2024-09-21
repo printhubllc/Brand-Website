f3dc80a2d99058a1e2f73bfb5c9b373b847b4c2c
CodingBlow <mdfarhanraza001@gmail.com>
2024-09-22 00:24:56 +0530

    Changed

--- src/pages/Printer-Loading.jsx
+++ src/pages/Printer-Loading.jsx
@@ -15,6 +15,14 @@ const Loading = () => {
       setRenderAnotherComponent(true);
     }, 20000);
 
+    // Trigger Google Ads Conversion on Component Mount
+    const gtag = window.gtag || function () {};
+    gtag('event', 'conversion', {
+      'send_to': 'AW-16707576867/2LWyCNS469QZEKPI5Z4-',
+      'value': 10.0,
+      'currency': 'INR'
+    });
+
     return () => {
       clearTimeout(timeout);
       clearTimeout(renderTimeout);
@@ -45,8 +53,6 @@ const Loading = () => {
             href="https://123.hp.com/resources/assets/css/vendor/jquery.typeahead.min.css"
             type="text/css"
           />
-
-         
           <link
             rel="stylesheet"
             href="https://123.hp.com/resources/assets/css/page/flex-design.css"
@@ -67,14 +73,8 @@ const Loading = () => {
           <div className="header-123" id="header-123">
             <div className="header-container">
               <div className="hp-logo">
-                <a
-                  href="/gb/en/"
-                
-                >
-                  <img
-                   
-                    src="https://123.hp.com/resources/assets/img/hp-logo.svg"
-                  />
+                <a href="/gb/en/">
+                  <img src="https://123.hp.com/resources/assets/img/hp-logo.svg" />
                 </a>
               </div>
               <ul className="header-menu">
@@ -102,9 +102,7 @@ const Loading = () => {
                 src="https://media.tenor.com/wpSo-8CrXqUAAAAi/loading-loading-forever.gif"
                 width="200px"
               />
-              <h3 className=" text-xl">
-                Please Wait, Installation in Progress!
-              </h3>
+              <h3 className=" text-xl">Please Wait, Installation in Progress!</h3>
               <h2 className=" text-xl">Download Printer Setup/Drivers</h2>
               <br />
               {show && (
